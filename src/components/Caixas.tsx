bd52f67d939ee199fd42972593051311fbe0d919
ramonespier <ramon.coelhomelo@outlook.com>
2024-08-31 20:21:47 -0300

    style: explorando classes com tailwind :)

--- src/components/Caixas.tsx
+++ src/components/Caixas.tsx
@@ -1,10 +1,13 @@
-export default function Caixas() {
+export default function Caixas(props: any) {
     return (
         <div className={`
+        flex justify-center items-center
         bg-neutral-600
-        w-1/3 h-96
+        w-1/4 h-96
+        text-5xl font-thin
         `}>
             Caixas
+            {100 * 100}  {/*Itens ou operadores dentro de caixas funcionam como comandos em javascript */}
         </div>
     )
 }
--- src/pages/basicos/caixas.tsx
+++ src/pages/basicos/caixas.tsx
@@ -6,6 +6,7 @@ export default function PaginaCaixa () {
             <Caixas />
             <Caixas />
             <Caixas />
+            <Caixas />
         </div>
     )
 }
